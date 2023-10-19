988c27b9c19fe8a7b08e2a2584f4fd4ea844c51c
alberto8812 <cvelascosaavedra@gmail.com>
2023-10-19 10:20:46 -0500

    filter listView

--- src/issues/hooks/useLabels.tsx
+++ src/issues/hooks/useLabels.tsx
@@ -15,7 +15,45 @@ const getLabels=async():Promise<Label[]>=>{
         ['label'],//definicmos el espacio en chache
         getLabels,
         {
-          staleTime:1000*60*60//la data se va a mantener fresa por una hora 
+         staleTime:1000*60*60,//la data se va a mantener fresa por una hora 
+        //  initialData:[
+        //   {
+        //     id: 69105383,
+        //     node_id: "MDU6TGFiZWw2OTEwNTM4Mw==",
+        //     url: "https://api.github.com/repos/facebook/react/labels/Browser:%20IE",
+        //     name: "Browser: IE",
+        //     color: "c7def8",
+        //     default: false,
+        //   },
+        //   {
+        //     id: 69105358,
+        //     node_id: "MDU6TGFiZWw2OTEwNTM1OA==",
+        //     url: "https://api.github.com/repos/facebook/react/labels/Browser:%20Safari",
+        //     name: "Browser: Safari",
+        //     color: "c7def8",
+        //     default: false,
+        //   }
+        // ],//da confianza en la data fresca 
+         placeholderData: [//debe ser un  objeto que lusca como la data que se esta cargando, muestra informacion antes
+          {
+            id: 69105383,
+            node_id: "MDU6TGFiZWw2OTEwNTM4Mw==",
+            url: "https://api.github.com/repos/facebook/react/labels/Browser:%20IE",
+            name: "Browser: IE",
+            color: "c7def8",
+            default: false,
+          },
+          {
+            id: 69105358,
+            node_id: "MDU6TGFiZWw2OTEwNTM1OA==",
+            url: "https://api.github.com/repos/facebook/react/labels/Browser:%20Safari",
+            name: "Browser: Safari",
+            color: "c7def8",
+            default: false,
+          }
+      ]
+          
+         
         }
       );
 
